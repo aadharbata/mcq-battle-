2899b9d9f8b6d067fd8706b4ddf952c3f0a8e5ac
Aadhar Batra <138890041+aadharbata@users.noreply.github.com>
2026-09-07 15:42:40 +0530

    Update next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next'
 
 const config: NextConfig = {
   reactStrictMode: false,
+  serverExternalPackages: ['@prisma/client', '@repo/db'],
 }
 
-export default config 
+export default config
